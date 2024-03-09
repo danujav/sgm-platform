fd8e96ee12bb91b10327c98a1d5c9b3a11e8968c
danujav <danujagreru@gmail.com>
2024-03-09 15:38:03 +0530

    express login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 // App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginForm from './LoginForm';
-import RegisterForm from './RegisterForm';
-import MemberDashboardForm from './MemberDashboardForm';
-import OwnerDashboardForm from './OwnerDashboardForm';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginForm from "./LoginForm";
+import RegisterForm from "./RegisterForm";
+import MemberDashboardForm from "./MemberDashboardForm";
+import OwnerDashboardForm from "./OwnerDashboardForm";
+import AddGymForm from "../src/AddGymForm";
+
+import  Home from "../src/pages/Dashboard";
+
+import MiniDrawer from "../src/pages/Layout/Drawer";
+import NoMatch from "../src/pages/404Page";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/register" element={<RegisterForm />} />
-        <Route path="/member-dashboard" element={<MemberDashboardForm />}/>
-        <Route path="/owner-dashboard" element={<OwnerDashboardForm />}/>
-      </Routes>
-    </Router>
+    <Routes>
+    <Route path="/" element={<MiniDrawer />}>
+      <Route index element={<Home />} />
+      <Route path="/add-gym" element={<AddGymForm />} />
+      {/* <Route path="/delete-gym" element={<Supplier />} />
+      <Route path="update-gym" element={<Orders />} /> */}
+      <Route path="*" element={<NoMatch />} />
+    </Route>
+  </Routes>
   );
 }
 
